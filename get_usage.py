a21b028b5baed08021488a99a0bc26162ffd4eca
Mohammed <mabdella@ualberta.ca>
2025-04-06 13:26:05 -0400

    Update get_usage.py

--- get_usage.py
+++ get_usage.py
@@ -10,7 +10,7 @@ def get_ollama_processes_usage(csv_writer):
         try:
             # Check if the process name contains "ollama"
             if 'ollama' in proc.info['name'].lower():
-                llm_model = os.getenv('llm_model', 'Not Set')  # Default to 'Not Set' if the variable is not found
+                llm_model = os.getenv('llm_model', 'Not Set')  
                 pid = proc.info['pid']
                 name = proc.info['name']
                 cpu_percent = proc.info['cpu_percent']
