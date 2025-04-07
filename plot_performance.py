e6cb3fce3bd700b43577780020731cf63e310338
Mohammed <mabdella@ualberta.ca>
2025-04-07 08:26:38 -0400

    Update plot_performance.py

--- plot_performance.py
+++ plot_performance.py
@@ -2,23 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-data = pd.read_csv('ollama_process_usage.csv') 
+data = pd.read_csv('ollama_process_usage.csv')
 
 data['Timestamp'] = pd.to_datetime(data['Timestamp'])
 
 if not os.path.exists('results'):
     os.makedirs('results')
 
-data['Relative Time (s)'] = 0  
+data['Relative Time (s)'] = 0
 
 for model, group in data.groupby('LLM Model'):
     group = group.sort_values(by='Timestamp')
-    start_time = group['Timestamp'].min()  
+    start_time = group['Timestamp'].min()
     print(start_time)
     data.loc[data['LLM Model'] == model, 'Relative Time (s)'] = (data['Timestamp'] - start_time).dt.total_seconds()
 
 print(data)
-
 
 plt.figure(figsize=(10, 6))
 
@@ -37,7 +36,7 @@ for model, group in data.groupby('LLM Model'):
 
 # # Display statistics on the plot
 # plt.text(0.5, 0.9, f'Mean: {cpu_mean:.2f}%\nMin: {cpu_min:.2f}%\nMax: {cpu_max:.2f}%', 
-         transform=plt.gca().transAxes, fontsize=12, va='top', ha='center')
+#          transform=plt.gca().transAxes, fontsize=12, va='top', ha='center')
 
 plt.tight_layout()
 plt.savefig('results/all_models_cpu_usage_relative_time.png')
